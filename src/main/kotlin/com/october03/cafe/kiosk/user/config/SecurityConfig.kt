033eced1abdf7f033029908f423b08888184e996
october03 <season_autumn@kakao.com>
2024-02-11 16:00:20 +0900

    Spring Security 설정

---
+++ src/main/kotlin/com/october03/cafe/kiosk/user/config/SecurityConfig.kt
@@ -0,0 +1,22 @@
+package com.october03.cafe.kiosk.user.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.invoke
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+  @Bean
+  open fun configure(http: HttpSecurity): SecurityFilterChain {
+    http {
+      csrf {
+        disable()
+      }
+    }
+    return http.build()
+  }
+}
